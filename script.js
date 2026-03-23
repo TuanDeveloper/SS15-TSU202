0c83cd4342928dcf885378dc00588d8e6dcf621d
Tuancoder2002 <it.coder.quoctuan@gmail.com>
2026-03-23 21:26:49 +0700

    done'

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-let std1 = {
-    name: "Hiền",
-    age: 22,
-    gender: "female"
+// Mảng 1 chiều
+let arr = [1, 2, 3, 4, 5]
+
+let arr2 = [[1, 2, 3], [4, 5, 6, 11, 10], [7, 8, 9]]
+
+// console.log(arr2[2][1]);
+
+for (let i = 0; i < arr2.length; i++) { // Lặp 3 lần qua từng phần tử mảng lớn
+    for (let j = 0; j < arr2[i].length; j++) { // Lặp qua từng phần tử ở mảng nhỏ
+        console.log(arr2[i][j]);
+    }
 }
-
-let std2 = {
-    name: "Nam",
-    age: 26,
-    gender: "male"
-}
-
-let students = [std1, std2, "Hậu"];
-
-console.log(); // Nam handsome 26 tuoi la male
