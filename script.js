f648075629803b13eb72ba944a51e05380298078
Tuancoder2002 <it.coder.quoctuan@gmail.com>
2026-03-23 20:42:20 +0700

    bai2

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-// Bài 1: Viết chương trình khởi tạo nhập vào một mảng số nguyên gồm 10 phần tử. 
-// Chương trình thực hiện tính và hiển thị xem có bao nhiêu số nguyên lớn hơn 
-// hoặc bằng 10.
+// Bài 2: Viết chương trình khởi tạo nhập vào một mảng số nguyên gồm 10 phần tử 
+// khác nhau. Chương trình hiển thị ra được phần tử có giá trị lớn nhất trong mảng và 
+// vị trí của phần tử đó.
 
-let numbers = []; // Mảng rỗng
-
-for (let i = 0; i < 10; i++) {
+let numbers = [];
+for (let i = 0; i < 5; i++) {
     let num = Number(prompt("Nhap vao 1 so nguyen"));
     numbers.push(num);
 }
-
-for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] >= 10) {
-
-        console.log(numbers[i]);
+// [1, 3, 5, 6, 7]
+let max = numbers[0]; // 1 --  Phần tử đầu tiên là lớn nhất
+let indexFind = 0;
+for (let i = 1; i < numbers.length; i++) {
+    if (max < numbers[i]) {
+        max = numbers[i];
+        indexFind = i;
     }
 }
 
+console.log("Gia tri lon nhat la:", max);
+console.log("Vi tri la:", indexFind);
 
