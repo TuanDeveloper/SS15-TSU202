883bc4244588fddc990672dfbd9eba1627ea050d
Tuancoder2002 <it.coder.quoctuan@gmail.com>
2026-03-23 21:04:07 +0700

    object

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-// Bài 2: Viết chương trình khởi tạo nhập vào một mảng số nguyên gồm 10 phần tử 
-// khác nhau. Chương trình hiển thị ra được phần tử có giá trị lớn nhất trong mảng và 
-// vị trí của phần tử đó.
-
-let numbers = [];
-for (let i = 0; i < 5; i++) {
-    let num = Number(prompt("Nhap vao 1 so nguyen"));
-    numbers.push(num);
-}
-// [1, 3, 5, 6, 7]
-let max = numbers[0]; // 1 --  Phần tử đầu tiên là lớn nhất
-let indexFind = 0;
-for (let i = 1; i < numbers.length; i++) {
-    if (max < numbers[i]) {
-        max = numbers[i];
-        indexFind = i;
-    }
+let std1 = {
+    name: "Hiền",
+    age: 22,
+    gender: "female"
 }
 
-console.log("Gia tri lon nhat la:", max);
-console.log("Vi tri la:", indexFind);
+let std2 = {
+    name: "Nam",
+    age: 26,
+    gender: "male"
+}
 
+let students = [std1, std2, "Hậu"];
+
+console.log(); // Nam handsome 26 tuoi la male
